8e8e6b78fcbc51ddea5537179736fbe1a5a0beb5
kyungtae <kthan@securityplatform.co.kr>
2017-05-08 15:19:44 +0900

    rename the test key file

--- boards/axio-builder-ms500/dist/flash.sh
+++ boards/axio-builder-ms500/dist/flash.sh
@@ -4,12 +4,9 @@ if [ `uname` = "Linux" ]; then
     echo "### Flashing axio-builder-ms500 ###"
 	BINFILE=${ELFFILE%.elf}.bin
 	SIGFILE=${ELFFILE%.elf}.bin.sig
-#SIGFILE=`basename ${ELFFILE%.elf}.bin.sig`
+	#SIGFILE=`basename ${ELFFILE%.elf}.bin.sig`
 	"${OBJCOPY}" --gap-fill=0xff -O binary "${ELFFILE}" "${BINFILE}"
-	echo "${SIGNFW}" "${BINFILE}" "${PKEY}"
 	"${SIGNFW}" "${BINFILE}" "${PKEY}"
-
-    echo "echo:${AXTOOL}" -f "${SIGFILE}" "${PORT}"
     "${AXTOOL}" -f "${SIGFILE}" "${PORT}"
 else
     echo "CAUTION: No flash tool for your host system found!"
